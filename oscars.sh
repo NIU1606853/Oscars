e08ab3e685fc051fecd0b73b1319e9f63a0b5b52
NIU1606853 <73660589+NIU1606853@users.noreply.github.com>
2020-12-03 18:11:31 +0100

    Creat Menú

--- oscars.sh
+++ oscars.sh
@@ -1,1 +1,27 @@
 #!/bin/bash
+
+echo -n "---------------------------------------------------------
+Base de Dades d’actors i actrius guanyadors/es de l’Oscar
+---------------------------------------------------------
+1. Històric d’Oscars.
+2. Qui va guanyar ...?
+3. Ets un Expert?
+4. Altes, baixes i modificacions.
+0. Sortir."
+
+echo -n "Escull una opció: $OPT"
+
+read $OPT
+
+
+
+case $OPT in 
+	1 | 2 | 3 | 4)
+	echo -n "En desenvolupament"
+	;;
+	
+	0)
+	echo -n "-"
+	;;
+	
+esac
