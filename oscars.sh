5f3f82b505469a2b1ecc5f04031ec2464a624d31
Hector <hector.garciape@e-campus.uab.cat>
2020-12-17 18:14:33 +0100

    Modificació d'arxiu oscars.sh

--- oscars.sh
+++ oscars.sh
@@ -1,27 +1,52 @@
 #!/bin/bash
 
-echo -n "---------------------------------------------------------
-Base de Dades d’actors i actrius guanyadors/es de l’Oscar
----------------------------------------------------------
-1. Històric d’Oscars.
-2. Qui va guanyar ...?
-3. Ets un Expert?
-4. Altes, baixes i modificacions.
-0. Sortir."
-
-echo -n "Escull una opció: $OPT"
-
-read $OPT
+function NoVALID ()
+{
+    echo "Error: Opció no valida"
+    sleep 3s
+    clear
+}
 
 
 
-case $OPT in 
-	1 | 2 | 3 | 4)
-	echo -n "En desenvolupament"
-	;;
-	
-	0)
-	echo -n "-"
-	;;
-	
+function MENU ()
+{
+echo 
+"
+--------------------------------------------------------- 
+Base de Dades d’actors i actrius guanyadors/es de l’Oscar  ---------------------------------------------------------
+1.Històric d’Oscars.
+2.Qui va guanyar ...? 
+3.Ets un Expert? 
+4.Altes, baixes i modificacions. 
+0.Sortir. 
+" 
+}
+
+function DESENVOLUPAMENT ()
+{
+case $opcio in 
+
+0) exit ;;
+
+1 | 2 | 3 | 4)
+    echo "En desenvolupament"
+    read -n 1 -s
+    clear
+;;
+
+*) NoVALID
+
 esac
+}
+
+while true 
+do
+
+MENU
+echo "Escull una opció: "
+read opcio
+
+DESENVOLUPAMENT
+
+done
